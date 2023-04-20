dfd790a24f73b8fdab32fc93295ab7b84d2a67a5
udonwajnr <udonwajnr10@gmail.com>
2023-04-20 17:04:01 +0100

    first commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Movie from "./Movie"
 
 export default async function Home() {
-  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`)
+  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`)
   const res = await data.json()
   console.log(res)
   
