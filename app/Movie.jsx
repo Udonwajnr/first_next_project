89ddd9fb244b24f5b23a565d951867988dd8099f
udonwajnr <udonwajnr10@gmail.com>
2023-04-20 17:09:19 +0100

    first commit

--- app/Movie.jsx
+++ app/Movie.jsx
@@ -5,8 +5,8 @@ export default function Movie({title,id,poster_path,release_date}){
     const imagePath = "https://image.tmdb.org/t/p/original"
     return (
         <div>
-            <h1>{title}</h1>
-            <h2>{release_date}</h2>
+            <h1 className="text-white">{title}</h1>
+            <h2 className="text-white">{release_date}</h2>
             <Link href={`${id}`}>
                 <Image src={imagePath+poster_path} width={800} height={800} alt={title}/>
             </Link>
